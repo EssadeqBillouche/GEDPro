30cd5a45fc6acb938ecad49b6293f41a82bf6bb5
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-22 17:06:09 +0100

    feat: integrate authentication module and configure database connections
    - Added AuthModule with AuthController and AuthService for handling authentication logic.
    - Integrated Mongoose for MongoDB and TypeORM for PostgreSQL in app.module.ts.
    - Configured Swagger for API documentation.
    - Updated package.json to include necessary dependencies for NestJS, Mongoose, TypeORM, and Swagger.
    - Created unit tests for AuthController and AuthService.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot("mongodb+srv://essadeqbillouche_db_user1:icz3Bwfdu0q66Tki@gedpro.7vd8iwn.mongodb.net/?appName=GEDPro"),
+
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port : 5432,
+      username : "postgres",
+      password : "11111",
+      database: 'GED_PRO',
+    }),
+
+    ConfigModule.forRoot(),
+
+    AuthModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
