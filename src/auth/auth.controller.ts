c0ba2708d305e72040eb79f4ec3320b24a95885a
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-23 16:15:07 +0100

    feat: implement registration endpoint with validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post , Body, ValidationPipe} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  
+  @Post('register')
+  register(@Body( new ValidationPipe(),) registerDto : RegisterDto)
+  {
+    try{
+          return this.authService.register(registerDto)
+    }catch(error){
+      return { message : error}
+    }
+  }
 }
