3cc1f063f6200008e6a8c2c83d97c1241fc7f956
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-23 16:15:33 +0100

    feat: implement AuthService with user registration and email lookup functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entites/user.entity';
+import { Role } from './entites/role.entity';
+import { Repository } from 'typeorm';
+import { Permession } from './entites/permession.entity';
+import { RegisterDto } from './dto/register.dto';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    
+    constructor( 
+        @InjectRepository(User)
+        private UserRepository : Repository <User>,
+        
+        @InjectRepository(Role)
+        private RoleRepository : Repository <Role>,
+        
+        @InjectRepository(Permession)
+        private PermissionRepisitory : Repository <Permession>,
+
+    ){}
+
+    async findByEmail(userEmail : string){
+        const user = this.UserRepository.findOne({where : { email : userEmail}})
+        return user
+    }
+
+    async login(email : string, password : string){
+
+    }
+    async register(userdata : RegisterDto) {
+
+        const user = this.findByEmail(userdata.email);
+
+        if(!user){
+            const newUser = this.UserRepository.create(userdata)
+            return await this.UserRepository.save(newUser);
+        }
+        throw new NotFoundException("this email a ready exist, try to login") 
+    }
+}
