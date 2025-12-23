7b82dc9ed900933db713899a4c869c8c8410b06f
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-23 16:15:26 +0100

    feat: add UserService and TypeORM configuration for role, user, and permission entities

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Role } from './entites/role.entity';
+import { User } from './entites/user.entity';
+import { Permession } from './entites/permession.entity';
+import { UserService } from './user/user.service';
 
 @Module({
+  imports : [
+    TypeOrmModule.forFeature([Role, User, Permession])
+  ]
+  ,
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserService],
 })
 export class AuthModule {}
