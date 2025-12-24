a460094e3cf0423bf40317d57a02701ed96f7631
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-24 14:58:05 +0100

    refactor: fix import path for RegisterDto and clean up AuthService methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,40 +4,49 @@ import { User } from './entites/user.entity';
 import { Role } from './entites/role.entity';
 import { Repository } from 'typeorm';
 import { Permession } from './entites/permession.entity';
-import { RegisterDto } from './dto/register.dto';
-
+import { RegisterDto } from './dtos/register.dto';
+import bcrypt from 'node_modules/bcryptjs';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private UserRepository: Repository<User>,
+
+    @InjectRepository(Role)
+    private RoleRepository: Repository<Role>,
+
+    @InjectRepository(Permession)
+    private PermissionRepisitory: Repository<Permession>,
+  ) {}
+
+  async findByEmail(userEmail: string) {
+    const user = await this.UserRepository.findOne({
+      where: { email: userEmail },
+    });
+    return user;
+  }
+
+  async login(email: string, password: string) {
+
+
     
-    constructor( 
-        @InjectRepository(User)
-        private UserRepository : Repository <User>,
-        
-        @InjectRepository(Role)
-        private RoleRepository : Repository <Role>,
-        
-        @InjectRepository(Permession)
-        private PermissionRepisitory : Repository <Permession>,
+  }
 
-    ){}
+  async register(userdata: RegisterDto) {
+    try {
+      const user = await this.findByEmail(userdata.email);
 
-    async findByEmail(userEmail : string){
-        const user = this.UserRepository.findOne({where : { email : userEmail}})
-        return user
+      const hashedPassword = await bcrypt.hash(userdata.password, 10);
+      userdata = { ...userdata, password: hashedPassword };
+
+      if (user === null) {
+        const newUser = this.UserRepository.create(userdata);
+        return await this.UserRepository.save(newUser);
+      }
+      throw new NotFoundException('this email a ready exist, try to login');
+    } catch (error) {
+      throw error;
     }
-
-    async login(email : string, password : string){
-
-    }
-    async register(userdata : RegisterDto) {
-
-        const user = this.findByEmail(userdata.email);
-
-        if(!user){
-            const newUser = this.UserRepository.create(userdata)
-            return await this.UserRepository.save(newUser);
-        }
-        throw new NotFoundException("this email a ready exist, try to login") 
-    }
+  }
 }
