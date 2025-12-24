5a4eb4ca847d862db8ee24eaba4b046ff89b233d
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-24 14:56:46 +0100

    feat: implement ErrorInterceptor for centralized error handling

---
+++ src/common/interceptors/error.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, InternalServerErrorException } from '@nestjs/common';
+import { error } from 'console';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { LogToFile } from 'src/logs/System.log';
+
+@Injectable()
+export class ErrorInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+        catchError((err)=>{
+          if(err instanceof HttpException){
+            // throw the http exceptions to user
+            throw err
+          }
+          console.error('errror with : ', err);
+          LogToFile(err); // log the error inside the file 
+          return throwError(()=> new InternalServerErrorException('unexpected error accured'))
+        })
+    )
+  }
+}
+
