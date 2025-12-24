b7722f673138fd2321b5438f4320ee16f7085c62
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-24 14:57:24 +0100

    refactor: clean up imports and adjust error interceptor usage in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import morgan from 'morgan';
+import { ErrorInterceptor } from './common/interceptors/error.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   app.use(morgan('dev'))
-
+  app.useGlobalInterceptors(new ErrorInterceptor()) // error interceptor
    // swager
   const config = new DocumentBuilder()
     .setTitle('GEDPro')
