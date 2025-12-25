8bbbc163003faabef7529312845902c8a20034db
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-25 16:56:19 +0100

    feat: add login and refreshToken endpoints to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Param, Post , Body, ValidationPipe, BadRequestException} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto';
+import { LoginDto } from './dtos/Login.dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body( new ValidationPipe(),) registerDto : RegisterDto, res : Response)
+  async register(@Body( new ValidationPipe()) registerDto : RegisterDto, res : Response)
   {
     try{
           return await this.authService.register(registerDto)
@@ -15,4 +17,18 @@ export class AuthController {
       throw new BadRequestException(error.message)
     }
   }
+
+  @Post('login')
+  async login( @Body() loginInfo : LoginDto){
+
+    return await this.authService.login(loginInfo)
+
+  }
+
+  @Post('refreshToken')
+  async refreshToken(){
+    
+  }
+
+
 }
