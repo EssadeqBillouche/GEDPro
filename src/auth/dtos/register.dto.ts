92c56a37fc9be5179d083209debe7507c66151e8
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-25 16:56:43 +0100

    fix: standardize formatting and remove unnecessary whitespace in RegisterDto

--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -1,19 +1,18 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from "class-validator"
-
-
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class RegisterDto {
+  @IsEmail()
+  readonly email: string;
 
-    @IsEmail()
-    readonly email : string;
+  @IsStrongPassword()
+  readonly password: string;
 
-     @IsStrongPassword()
-     readonly password : string;
+  @IsNotEmpty()
+  readonly firstName: string;
 
-     @IsNotEmpty()
-     readonly firstName : string;
+  @IsNotEmpty()
+  readonly lastName: string;
 
-     @IsNotEmpty()
-    readonly lastName : string;
-
+  @IsNotEmpty()
+  readonly role: string;
 }
