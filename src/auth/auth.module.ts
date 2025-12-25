fa0584a97e95fc867c606f6a39229382ee3df243
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-25 16:56:25 +0100

    fix: correct import paths for entity files and configure JWT module in AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Role } from './entites/role.entity';
-import { User } from './entites/user.entity';
-import { Permession } from './entites/permession.entity';
+import { Role } from './Entites/Role.entity';
+import { User } from './Entites/User.entity';
+import { Permession } from './Entites/Permession.entity';
 import { UserService } from './user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+
+dotenv.config()
 
 @Module({
   imports : [
-    TypeOrmModule.forFeature([Role, User, Permession])
+    TypeOrmModule.forFeature([Role, User, Permession]),
+    JwtModule.register({
+      secret : process.env.JWTSECRET,
+      signOptions : {expiresIn : '1d'}
+    })
   ]
   ,
   controllers: [AuthController],
