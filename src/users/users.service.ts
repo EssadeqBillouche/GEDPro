e21847029be80aff514141eee00d85d9e9d69bab
essadeq billouche <essadeq.billouche@gmail.com>
2025-12-29 11:58:36 +0100

    feat: implement UsersService with user, role, and permission management methods

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Permission } from 'src/auth/Entites/Permission.entity';
+import { Role } from 'src/auth/Entites/Role.entity';
+import { User } from 'src/auth/Entites/User.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        
+    @InjectRepository(User)
+    private UserRepository : Repository<User>,
+
+    @InjectRepository(Role)
+    private RoleRepository : Repository<Role>,
+
+    @InjectRepository(Permission)
+    private PermissionRepository : Repository<Permission>
+    
+
+){}
+
+async findByEmail(userEmail: string) {
+    const user = await this.UserRepository.findOne({
+      where: { email: userEmail },
+    });
+    return user;
+  }
+
+
+  async getRole(role_Name : string){
+    return await this.RoleRepository.findOne({where : {name : role_Name}})
+  }
+
+  async createUser( userData : Partial <User>){
+
+    const registeredUser = this.UserRepository.create(userData);
+    return await this.UserRepository.save(registeredUser)
+
+  }
+}
