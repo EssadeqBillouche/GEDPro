79a093d316e2e977d5fafa18a84365a076395083
essadeq billouche <essadeq.billouche@gmail.com>
2026-01-05 10:03:45 +0100

    refactor: improve formatting and organization of UsersService methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,40 +3,53 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Permission } from 'src/auth/Entites/Permission.entity';
 import { Role } from 'src/auth/Entites/Role.entity';
 import { User } from 'src/auth/Entites/User.entity';
+import { Organization } from 'src/auth/Entites/Organization.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        
+  constructor(
     @InjectRepository(User)
-    private UserRepository : Repository<User>,
+    private UserRepository: Repository<User>,
 
     @InjectRepository(Role)
-    private RoleRepository : Repository<Role>,
+    private RoleRepository: Repository<Role>,
 
     @InjectRepository(Permission)
-    private PermissionRepository : Repository<Permission>
-    
+    private PermissionRepository: Repository<Permission>,
 
-){}
+    @InjectRepository(Organization)
+    private OrganizationRepository: Repository<Organization>,
+  ) {}
 
-async findByEmail(userEmail: string) {
-    const user = await this.UserRepository.findOne({
+  async findByEmail(userEmail: string) {
+    return await this.UserRepository.findOne({
       where: { email: userEmail },
+      relations: ['roles', 'organization'],
     });
-    return user;
   }
 
-
-  async getRole(role_Name : string){
-    return await this.RoleRepository.findOne({where : {name : role_Name}})
+  async findById(id: string) {
+    return await this.UserRepository.findOne({
+      where: { id },
+      relations: ['roles', 'organization'],
+    });
   }
 
-  async createUser( userData ){
+  async getRoleBySlug(slug: string) {
+    return await this.RoleRepository.findOne({ where: { slug } });
+  }
 
-    const registeredUser = this.UserRepository.create(userData);
-    return await this.UserRepository.save(registeredUser)
+  async createUser(userData: Partial<User>) {
+    const newUser = this.UserRepository.create(userData);
+    return await this.UserRepository.save(newUser);
+  }
 
+  async updateRefreshToken(userId: string, refreshTokenHash: string) {
+    await this.UserRepository.update(userId, { refreshTokenHash });
+  }
+
+  async updateLastLogin(userId: string) {
+    await this.UserRepository.update(userId, { lastLogin: new Date() });
   }
 }
