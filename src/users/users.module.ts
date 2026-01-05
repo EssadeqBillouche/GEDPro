5eae0b2af08278698ba3c255cf4d54dadc89e47c
essadeq billouche <essadeq.billouche@gmail.com>
2026-01-05 10:03:41 +0100

    refactor: reorganize imports and formatting in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
 import { User } from 'src/auth/Entites/User.entity';
 import { Role } from 'src/auth/Entites/Role.entity';
 import { Permission } from 'src/auth/Entites/Permission.entity';
+import { Organization } from 'src/auth/Entites/Organization.entity';
 
 @Module({
-    imports : [TypeOrmModule.forFeature([User,Role,Permission])],
-    providers : [UsersService],
-    exports : [UsersService]
+  imports: [TypeOrmModule.forFeature([User, Role, Permission, Organization])],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
