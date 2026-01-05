4cfe9e87f3a19428ee47abbe70bcb33c8393303b
essadeq billouche <essadeq.billouche@gmail.com>
2026-01-05 10:03:13 +0100

    feat: refactor JwtStrategy to improve user validation and remove unused dependencies

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,28 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { User } from './Entites/User.entity';
-import { Repository } from 'typeorm';
-
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private configService: ConfigService, @InjectRepository(User) private userRepository : Repository<User>)  {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('JWTSECRET'),
+      secretOrKey: configService.get<string>('JWTSECRET'),
     });
   }
 
   async validate(payload: any) {
-
-    const {userId } = payload
-    const user = this.userRepository.findOne({where : {id : userId}})
-    
-    if(!user)
-    return { userId: payload.sub, username: payload.username };
+    const user = await this.usersService.findById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
