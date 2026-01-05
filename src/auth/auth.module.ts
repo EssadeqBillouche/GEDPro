789224b20341d92860a20263468c4ff5419bb03c
essadeq billouche <essadeq.billouche@gmail.com>
2026-01-05 10:03:01 +0100

    feat: refactor AuthModule to use async JWT configuration with ConfigService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,19 +2,26 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import * as dotenv from 'dotenv';
 import { UsersModule } from '../users/users.module';
-
-dotenv.config()
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports : [UsersModule,
-    JwtModule.register({
-      secret : process.env.JWTSECRET,
-      signOptions : {expiresIn : '1d'}
-    })
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWTSECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
