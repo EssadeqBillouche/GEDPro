5abf9d445a316e73f83a5c1477b57d314acd7ad3
essadeq billouche <essadeq.billouche@gmail.com>
2026-01-12 06:18:59 +0100

    refactor: update ESLint configuration rules for TypeScript and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,10 +26,18 @@ export default tseslint.config(
   },
   {
     rules: {
+      '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+    },
+  },
+  {
+    rules: {
+      quotes: ['error', 'single'],
+      'prettier/prettier': ['error'],
     },
   },
 );
